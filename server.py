d0e8bf853d463384c5b5c5706cf682e664c30c5b
Adrian Mora <adriandariom@gmail.com>
2017-05-05 17:02:23 -0400

    Server implements basic storage and retreival of files

--- server.py
+++ server.py
@@ -1,9 +1,22 @@
 from flask import Flask
+from flask import request
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return "Hello world!"
+stored_files = []
+
+@app.route('/<int:id_num>', methods=['GET'])
+def get_file(id_num):
+    return stored_files[id_num]
+
+
+@app.route('/', methods=['POST'])
+def upload_file():
+    data = request.args
+    new_file = data['uploaded_file']
+    stored_files.append(new_file)
+    return "File stored"
+
+
 
 if __name__ == "__main__":
     app.run()
