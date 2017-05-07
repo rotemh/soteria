0de0e642d74541cdec9768e405a3d63257907481
Adrian Mora <adriandariom@gmail.com>
2017-05-07 13:01:12 -0400

    Server stores and returns files

--- server.py
+++ server.py
@@ -1,21 +1,34 @@
 from flask import Flask
 from flask import request
+import hashlib
+import json
 app = Flask(__name__)
 
-stored_files = []
+stored_files = {}
 
-@app.route('/<int:id_num>', methods=['GET'])
-def get_file(id_num):
-    return stored_files[id_num]
+
+@app.route('/', methods=['GET'])
+def get_dummy_files():
+    print stored_files
+    return "Returned dummy files\n"
+
+
+@app.route('/<int:id>', methods=['GET'])
+def get_file(id):
+    if id in stored_files:
+        return stored_files[id]
+    else:
+        return "No such file\n"
 
 
 @app.route('/', methods=['POST'])
 def upload_file():
-    data = request.args
-    new_file = data['uploaded_file']
-    stored_files.append(new_file)
-    return "File stored"
-
+    data = json.loads(request.data)
+    uploaded_file = data['uploaded_file']
+    salt = data['salt']
+    id = hashlib.md5(uploaded_file.encode()).hexdigest()
+    stored_files[id] = (uploaded_file, salt)
+    return "File stored\n"
 
 
 if __name__ == "__main__":
